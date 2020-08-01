a0c0da84147efc6301b2ec325566ea479ff3966d
Walter Haynes <walter-haynes@outlook.com>
2020-08-01 14:50:09 +0200

    Basic PlayerBehaviour Finished

---
+++ Assets/-Source-/Scripts/Core/Player/PlayerBehaviour.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Player
+{
+    using GenericAbilitySystem;
+
+    /// <summary> Inherit from this if you want a MonoBehaviour with access to your CharacterCore. </summary>
+    public abstract partial class PlayerBehaviour : AbilitySystemBehaviour<PlayerCore>
+    {
+        //No code needed thus far, it's all done in the parent.
+    }
+}
